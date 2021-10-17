36218de167c2e4bc6055a4fe3cdcb68fd78c043e
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2021-10-17 15:09:27 +1100

    Add app-routing.module.ts

--- starter/src/app/app.module.ts
+++ starter/src/app/app.module.ts
@@ -3,7 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms'
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { GitHubService } from './service/github.service';
 
 @NgModule({
@@ -12,6 +14,7 @@ import { GitHubService } from './service/github.service';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     HttpClientModule,
     FormsModule
   ],
